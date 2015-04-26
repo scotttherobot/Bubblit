8b8189165fbe5fdbeab866bb84b2cc05fc4681af
VeryBueno <lcuellar@calpoly.edu>
2015-04-26 19:58:02 +0000

    add CSV reading for looking up student names. binary images were too eroded. removed the logic in getBinaryImage that did that. output files are now in the same directory as PDFs some stylechecking

--- ExamGrader/src/com/universalquantification/examgrader/GradeExams.java
+++ ExamGrader/src/com/universalquantification/examgrader/GradeExams.java
@@ -14,10 +14,12 @@ import boofcv.struct.image.ImageFloat32;
 import boofcv.struct.image.ImageUInt8;
 import com.sun.pdfview.PDFFile;
 import com.sun.pdfview.PDFPage;
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Rectangle;
+import java.awt.Stroke;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
@@ -26,6 +28,8 @@ import java.io.IOException;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -41,27 +45,36 @@ import javax.swing.SwingWorker;
  *
  * @author luis
  */
-public class GradeExams extends SwingWorker<Void, Void> 
+public class GradeExams extends SwingWorker<Void, Void>
 {
     private ArrayList<String> paths;
-    
-    public GradeExams(ArrayList<String> filePaths){
+    private String csvPath;
+
+    public GradeExams(ArrayList<String> filePaths, String csvPath)
+    {
         this.paths = filePaths;
+        if (csvPath != null)
+        {
+            this.csvPath = csvPath;
+        }
     }
-    
+
     /*
      * Executed in event dispatching thread
      */
     @Override
-    public void done() {
-        String completeMessage = "Grading complete! You can find the score files"
-                 + "in the original directories of your graded PDF files."
+    public void done()
+    {
+        String completeMessage
+                = "Grading complete! You can find the score files"
+                + "in the original directories of your graded PDF files."
                 + "\n\n The directories are:\n\n";
-        
-        for (String s : paths) {
+
+        for (String s : paths)
+        {
             completeMessage += ("\n" + s);
         }
-        
+
         JOptionPane.showMessageDialog(null, completeMessage);
     }
 
@@ -71,7 +84,8 @@ public class GradeExams extends SwingWorker<Void, Void>
      * @param page a PDFPage
      * @return a BufferedImage
      */
-    public static BufferedImage createImageFromPDF(PDFPage page) {
+    public static BufferedImage createImageFromPDF(PDFPage page)
+    {
         // Size of the image to process. Smaller means faster, but less precise.
         int width = 1003;
         int height = 1298;
@@ -101,7 +115,8 @@ public class GradeExams extends SwingWorker<Void, Void>
      * @return a bounds object with the bounds
      */
     private static Bounds getFormBounds(ImageFloat32 image,
-            ImageFloat32 leftAnchor, ImageFloat32 rightAnchor) {
+            ImageFloat32 leftAnchor, ImageFloat32 rightAnchor)
+    {
         ImagePoint leftAnchorLoc = getTemplateLocation(image, leftAnchor, 1);
         ImagePoint rightAnchorLoc = getTemplateLocation(image, rightAnchor, 1);
 
@@ -123,7 +138,8 @@ public class GradeExams extends SwingWorker<Void, Void>
      * @return a bounds object with the bounds
      */
     private static Bounds getFormBounds(BufferedImage image,
-            BufferedImage leftAnchor, BufferedImage rightAnchor) {
+            BufferedImage leftAnchor, BufferedImage rightAnchor)
+    {
         // convert to usable format
         ImageFloat32 src = new ImageFloat32(image.getWidth(), image.getHeight());
         ImageFloat32 left = new ImageFloat32(leftAnchor.getWidth(),
@@ -148,7 +164,8 @@ public class GradeExams extends SwingWorker<Void, Void>
      */
     private static ImagePoint getTemplateLocation(ImageFloat32 image,
             ImageFloat32 template,
-            int expectedMatches) {
+            int expectedMatches)
+    {
 
         // initiate the template matcher
         TemplateMatching<ImageFloat32> matcher
@@ -177,26 +194,28 @@ public class GradeExams extends SwingWorker<Void, Void>
      * @param image input image
      * @return the resulting black-and-white image
      */
-    private static BufferedImage getBinaryImage(BufferedImage image) {
+    private static BufferedImage getBinaryImage(BufferedImage image)
+    {
         // convert into a usable format
-        ImageFloat32 input = ConvertBufferedImage.convertFromSingle(image, null,
-                ImageFloat32.class);
-        ImageUInt8 binary = new ImageUInt8(input.width, input.height);
-
-        // Select a global threshold using Otsu's method.
-        double threshold = GThresholdImageOps.computeOtsu(input, 0, 256);
-
-        // Apply the threshold to create a binary image
-        ThresholdImageOps.threshold(input, binary, (float) threshold, true);
-
-        // remove small blobs through erosion and dilation
-        ImageUInt8 filtered = BinaryImageOps.erode8(binary, 1, null);
-        filtered = BinaryImageOps.dilate8(filtered, 1, null);
-
-        BufferedImage visualFiltered = VisualizeBinaryData.renderBinary(
-                filtered, null);
-
-        return visualFiltered;
+//        ImageFloat32 input = ConvertBufferedImage.convertFromSingle(image, null,
+//                ImageFloat32.class);
+//        ImageUInt8 binary = new ImageUInt8(input.width, input.height);
+//
+//        // Select a global threshold using Otsu's method.
+//        double threshold = GThresholdImageOps.computeOtsu(input, 0, 256);
+//
+//        // Apply the threshold to create a binary image
+//        ThresholdImageOps.threshold(input, binary, (float) threshold, true);
+//
+//        // remove small blobs through erosion and dilation
+//        ImageUInt8 filtered = BinaryImageOps.erode8(binary, 1, null);
+//        filtered = BinaryImageOps.dilate8(filtered, 1, null);
+//
+//        BufferedImage visualFiltered = VisualizeBinaryData.renderBinary(
+//                filtered, null);
+//
+//        return visualFiltered;
+        return image;
     }
 
     /**
@@ -207,12 +226,14 @@ public class GradeExams extends SwingWorker<Void, Void>
      * @return a list of questions.
      */
     private static Exam detectExam(BufferedImage img,
-            Bounds bounds) {
+            Bounds bounds)
+    {
         ArrayList<Question> questions;
         HashMap<Integer, Question> qmap = new HashMap<Integer, Question>();
         int[] studentID = new int[9];
         String[] letters
-                = {
+                =
+                {
                     "A", "B", "C", "D", "E"
                 };
 
@@ -227,9 +248,11 @@ public class GradeExams extends SwingWorker<Void, Void>
         int black = img.getRGB(3, 3);
 
         // go through every cell and determine if it has a bubble in it.
-        for (double onColumn = 0; onColumn < numColumns; onColumn++) {
+        for (double onColumn = 0; onColumn < numColumns; onColumn++)
+        {
             // go through every row
-            for (double onRow = 0; onRow < numRows; onRow++) {
+            for (double onRow = 0; onRow < numRows; onRow++)
+            {
                 int x0 = (int) (bounds.getMinX() + onColumn * cellWidth);
                 int x1 = x0 + (int) cellWidth;
                 int y0 = (int) (bounds.getMinY() + onRow * cellHeight);
@@ -247,16 +270,19 @@ public class GradeExams extends SwingWorker<Void, Void>
                 //graphics.drawLine(x0, y1, x0, y0);
                 double numBlacks = 0;
                 // count the number of black pixels in the image
-                for (int color : colors) {
+                for (int color : colors)
+                {
                     // is the color black
-                    if (color == black) {
+                    if (color == black)
+                    {
                         numBlacks++;
                     }
                 }
 
                 // if the number of black pixels is over a threshold, then it
                 // is filled in.
-                if (numBlacks / (cellWidth * cellWidth) < .85) {
+                if (numBlacks / (cellWidth * cellWidth) < .85)
+                {
                     int colNum = (int) onColumn + 1;
                     int rowNum = (int) onRow + 1;
 
@@ -266,33 +292,41 @@ public class GradeExams extends SwingWorker<Void, Void>
                     boolean valid = false;
 
                     // first column of questions
-                    if (colNum >= 11 && colNum <= 15) {
+                    if (colNum >= 11 && colNum <= 15)
+                    {
                         startQ = 0;
                         choice = colNum - 11;
                         valid = true;
                     } // second column of questions
-                    else if (colNum >= 17 && colNum <= 21) {
+                    else if (colNum >= 17 && colNum <= 21)
+                    {
                         startQ = 35;
                         choice = colNum - 17;
                         valid = true;
                     } // third column of questions
-                    else if (colNum >= 23 && colNum <= 27) {
+                    else if (colNum >= 23 && colNum <= 27)
+                    {
                         startQ = 70;
                         choice = colNum - 23;
                         valid = true;
                     } // the user id field
                     else if (colNum >= 1 && colNum <= 9 && rowNum >= 11
-                            && rowNum <= 20) {
+                            && rowNum <= 20)
+                    {
                         studentID[colNum - 1] = rowNum - 11;
                     }
 
                     // create the question
                     int qNum = startQ + rowNum;
-                    if (valid) {
+                    if (valid)
+                    {
                         Question question;
-                        if (qmap.containsKey(qNum)) {
+                        if (qmap.containsKey(qNum))
+                        {
                             question = qmap.get(qNum);
-                        } else {
+                        }
+                        else
+                        {
                             question = new Question();
                         }
                         question.setqNum(startQ + rowNum);
@@ -300,6 +334,11 @@ public class GradeExams extends SwingWorker<Void, Void>
                         qmap.put(qNum, question);
                     }
 
+                    // uncomment below to draw a red line through
+                    // candidate blobs
+//                    Graphics2D graphics = img.createGraphics();
+//                    graphics.setColor(Color.red);
+//                    graphics.drawLine(x0, y0, x1, y1);
                 }
             }
         }
@@ -308,17 +347,22 @@ public class GradeExams extends SwingWorker<Void, Void>
         Exam exam = new Exam(questions);
         exam.setStudentID(studentID);
 
-        // uncomment below to write out the b&w image with grid lines
-        //File outputfile = new File("image" + exam.getStudentID() + ".jpg");
-        //try {
-        //    ImageIO.write(img, "jpg", outputfile);
-        //} catch (IOException ex) {
-        //    Logger.getLogger(GradeExams.class.getName()).log(Level.SEVERE, null, ex);
-        //}
+        // uncomment below to write out the b&w image with grid lines/blobs
+//        File outputfile = new File("image" + exam.getStudentID() + ".jpg");
+//        try
+//        {
+//            ImageIO.write(img, "jpg", outputfile);
+//        }
+//        catch (IOException ex)
+//        {
+//            Logger.getLogger(GradeExams.class.getName()).log(Level.SEVERE, null,
+//                    ex);
+//        }
         return exam;
     }
 
-    private static BufferedImage loadScaledImage(String path, int width) {
+    private static BufferedImage loadScaledImage(String path, int width)
+    {
         BufferedImage image = UtilImageIO.loadImage(path);
         BufferedImage scaled = new BufferedImage(width, width,
                 BufferedImage.TYPE_BYTE_BINARY);
@@ -329,7 +373,8 @@ public class GradeExams extends SwingWorker<Void, Void>
     }
 
     private static PDFFile loadPDF(String path) throws FileNotFoundException,
-            IOException {
+            IOException
+    {
         File pdfFile = new File(path);
         RandomAccessFile raf = new RandomAccessFile(pdfFile, "r");
         FileChannel channel = raf.getChannel();
@@ -344,27 +389,52 @@ public class GradeExams extends SwingWorker<Void, Void>
      * @param paths list of paths to PDFs
      */
     @Override
-    public Void doInBackground() {
+    public Void doInBackground()
+    {
         int progress = 2;
         setProgress(progress);
-        int fileDeltaProgress = 98/paths.size();
+        int fileDeltaProgress = 98 / paths.size();
+        CSVReader studentReader = null;
 
-        for (String path : paths) {
+        if (this.csvPath != null)
+        {
+            try
+            {
+                studentReader = new CSVReader(this.csvPath);
+                //studentReader.
+            }
+            catch (Exception ex)
+            {
+                System.out.println("Error loading CSV file" + ex.getMessage());
+            }
+        }
+
+        for (String path : paths)
+        {
             System.out.println("Process " + path);
+            Path pathInfo = Paths.get(path);
+            Path parentPath = pathInfo.getParent();
+            String sep = pathInfo.getFileSystem().getSeparator();
+            String outPath = parentPath.toAbsolutePath() + sep + pathInfo.
+                    getFileName().toString().split("\\.")[0];
+            System.out.println("outpat hsi " + outPath);
 
             PDFFile file = null;
 
             // load the pdf file
-            try {
+            try
+            {
                 file = loadPDF(path);
-            } catch (IOException ex) {
+            }
+            catch (IOException ex)
+            {
                 Logger.getLogger(GradeExams.class.getName()).log(Level.SEVERE,
                         "File " + path + " Not Found", ex);
                 System.exit(1);
             }
 
-            int pageDeltaProgress = fileDeltaProgress/(file.getNumPages() * 2);
-            
+            int pageDeltaProgress = fileDeltaProgress / (file.getNumPages() * 2);
+
             // get the first page to set dimensions.
             BufferedImage firstPageImage = createImageFromPDF(file.getPage(0));
             int newAnchorWidth = (int) (2 * ((firstPageImage.getWidth() / 8.5)
@@ -382,7 +452,8 @@ public class GradeExams extends SwingWorker<Void, Void>
 
             // go through every page in the pdf and grade the exam on it.
             int numPages = file.getNumPages() + 1;
-            for (int onPage = 1; onPage < numPages; onPage++) {
+            for (int onPage = 1; onPage < numPages; onPage++)
+            {
                 PDFPage page = file.getPage(onPage);
                 BufferedImage pageImage = createImageFromPDF(page);
 
@@ -393,13 +464,16 @@ public class GradeExams extends SwingWorker<Void, Void>
                 // get the questions and student ID
                 Exam exam = detectExam(getBinaryImage(pageImage), bounds);
                 // if this is the first exam, set it as the key.
-                if (onPage == 1) {
+                if (onPage == 1)
+                {
                     key = exam;
                     System.out.println("Answer Key: " + exam.toString());
-                } else {
+                }
+                else
+                {
                     exams.add(exam);
                 }
-                
+
                 progress += pageDeltaProgress;
                 setProgress(progress);
             }
@@ -407,57 +481,73 @@ public class GradeExams extends SwingWorker<Void, Void>
             // set up the answer key for grading
             ArrayList<Question> answerKey = key.getAnswers();
 
-            String[] scoreCols = {
+            String[] scoreCols =
+            {
                 "ID", "Name", "Correct"
             };
-            
+
             List<String[]> results = new ArrayList<>();
-            
+
             // grade every exam according to the key.
-            for (Exam exam : exams) {
+            for (Exam exam : exams)
+            {
                 exam.grade(answerKey);
                 System.out.println("Exam: " + exam.toString(true));
                 // CSV score reporting
                 Integer raw = exam.rawScore();
-                String name = "Student " + exam.getStudentID();
+                String name;
+                if (studentReader != null)
+                {
+                    HashMap<String, String> studentRecord = studentReader.
+                            getRecordByColumnValue("ID", Integer.toString(
+                                            exam.getStudentID()));
+                    name = studentRecord.get("Name");
+                }
+                else
+                {
+                    name = "Student " + exam.getStudentID();
+                }
 
-                String[] result = {
+                String[] result =
+                {
                     String.valueOf(exam.getStudentID()), name, raw.toString()
                 };
                 results.add(result);
-                
+
                 progress += pageDeltaProgress;
                 setProgress(progress);
             }
-            
-            writeCSV(path + "_scores_by_scan_order.csv",
+
+            writeCSV(outPath + "_scores_by_scan_order.csv",
                     new Report(scoreCols, results));
-            
-            Collections.sort(results, new Comparator<String[]>() {
+
+            Collections.sort(results, new Comparator<String[]>()
+            {
                 public int compare(String[] a, String[] b)
                 {
                     return a[0].compareTo(b[2]);
-                }      
+                }
             });
-            
-            writeCSV(path + "_scores_by_id.csv",
+
+            writeCSV(outPath + "_scores_by_id.csv",
                     new Report(scoreCols, results));
 
             Report statsReport = new StatsReporter().getReport(
                     exams, answerKey);
-            writeCSV(path + "_stats.csv", statsReport);
-            
+            writeCSV(outPath + "_stats.csv", statsReport);
+
         }
         setProgress(100);
         return null;
     }
-    
+
     private void writeCSV(String filename, Report report)
-    {   
+    {
         CSVWriter writer = new CSVWriter(filename);
         writer.setColumns(report.headers);
-        for (String[] row: report.rows) {
-                writer.addLine(row);
+        for (String[] row : report.rows)
+        {
+            writer.addLine(row);
         }
         writer.writeFile();
     }
