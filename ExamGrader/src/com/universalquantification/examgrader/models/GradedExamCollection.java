0397ab93547b1475efae613f81c5df8ded180110
Jenny Wang <ywang227@gmail.com>
2015-05-14 05:03:23 +0000

    GradedExamCollection: add pseudocode

--- ExamGrader/src/com/universalquantification/examgrader/models/GradedExamCollection.java
+++ ExamGrader/src/com/universalquantification/examgrader/models/GradedExamCollection.java
@@ -41,9 +41,19 @@ public class GradedExamCollection
      * @param exams List a list of Exams to be scored and saved in the
      * collection
      * @throws IllegalArgumentError if exams is empty
+     * @throws IllegalStateException if trying to grade an already-graded Exam
      */
     public GradedExamCollection(Exam answerKey, List<Exam> exams)
     {
+        // IF exams is empty
+            // RAISE IllegalArgumentException
+        
+        // FOR each exam in exams
+            // CALL grade Exam with answerKey   
+        // END FOR
+        
+        // SET answerKey field to answerKey
+        // SET exams field to exams
     }
 
     /**
@@ -52,6 +62,7 @@ public class GradedExamCollection
      */
     public Exam getAnswerKey()
     {
+        // RETURN answerKey
         return null;
     }
     
@@ -59,12 +70,13 @@ public class GradedExamCollection
      * Returns the nth Exam in the collection. A GradedExam will have the 
      * same position as the Exam it was scored from in the list that was passed
      * in the constructor.
-     * @param n int 0-indexed integer representing the position of the GradedExam
+     * @param n 0-indexed integer representing the position of the GradedExam
      * @return the nth Exam
      * @throws IndexOutOfBoundsException if there is no nth GradedExam
      */
     public Exam get(int n)
     {
+        // CALL get studentExams WITH n
         return null;
     }
 
@@ -74,6 +86,7 @@ public class GradedExamCollection
      */
     public Set<Integer> getQuestions()
     {
+        // CALL getQuestions exams
         return null;
     }
 
@@ -83,6 +96,7 @@ public class GradedExamCollection
      */
     public int getNumExams()
     {
+        // CALL size studentExams
         return 0;
     }
 
@@ -92,6 +106,13 @@ public class GradedExamCollection
      */
     public double getAverageScore()
     {
+        // SET sum to 0
+        // FOR exam in studentExams
+            // CALL getRawScore exam RETURNING examScore
+            // ADD examScore TO sum
+        // END FOR
+        
+        // RETURN sum divided by number of exams
         return 0;
     }
 
@@ -101,6 +122,18 @@ public class GradedExamCollection
      */
     public double getStdDeviation()
     {
+        // CALL getAverageScore RETURNING averageScore 
+        // SET sumOfSquares to 0
+        // FOR exam in exams
+            // CALL getRawScore exam RETURNING examScore
+            // SET difference to the difference between examScore and
+                // averageScore to sumOfSquares
+            // ADD difference squared to sumOfSquares        
+        // END FOR
+        
+        // RETURN the square root of sumOfSquares divided by one less than
+        // the number of exams 
+
         return 0;
     }
 
@@ -114,6 +147,17 @@ public class GradedExamCollection
      */
     public Map<Answer, Integer> getAnswerFrequency(int question)
     {
+        // INIT frequencies map
+        
+        // FOR exam in studentExams
+            // CALL getAnswer exam WITH question RETURNING answer
+            // IF frequencies does not contain a key for answer
+                // SET frequencies[answer] to 1
+            // ELSE
+                // SET frequencies[answer] to frequencies[answer] + 1
+        // END FOR
+        
+        // RETURN frequency
         return null;
     }
 
@@ -124,6 +168,9 @@ public class GradedExamCollection
     @Override
     public int hashCode()
     {
+        // CALL hashCode on studentExams RETURNING studentExamCode
+        // CALL hashCode on answerKey RETURNING answerKeyCode
+        // RETURN answerKeyCode * 31 + studentExamCode
         return -1;
     }
     
@@ -134,6 +181,15 @@ public class GradedExamCollection
     @Override
     public boolean equals(Object o)
     {
+        
+        // IF other is null
+            // RETURN false
+        // ELSE IF other is not a GradedExamCollection
+            // RETURN FALSE
+        // ELSE
+            // RETURN true iff this object's answerKey is equal to other's
+                // AND this object's studentExams are equal to other's
+        // END IF
         return false;
     }
     
@@ -144,6 +200,10 @@ public class GradedExamCollection
     @Override
     public String toString()
     {
+        // CALL toString answerKey RETURNING answerKeyOutput
+        // CALL toString studentExams RETURNING studentExamsOutput
+        // RETURN 'GradedExamCollection(' concatenated with answerKey
+            // concatenated with studentExams concatenated with ')'
         return null;
     }
     
