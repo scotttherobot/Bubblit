0410f26867f0124479f3c9e2cf0a35d97c934363
VeryBueno <lcuellar@calpoly.edu>
2015-05-22 22:27:06 +0000

    add first/last name fields to student

--- ExamGrader/src/com/universalquantification/examgrader/models/Student.java
+++ ExamGrader/src/com/universalquantification/examgrader/models/Student.java
@@ -1,107 +1,147 @@
 package com.universalquantification.examgrader.models;
 
+import com.universalquantification.examgrader.reader.StochasticString;
+import java.awt.image.BufferedImage;
+
 /**
- * Student represents the information for a given student. It can have a 
- * first name, last name, and identification number. A student record can also
- * be printed for convenience.
- * 
+ * Student represents the information for a given student. It can have a first
+ * name, last name, and identification number. A student record can also be
+ * printed for convenience.
+ *
  * @author C.Y. Tan
  */
-public class Student 
+public class Student
 {
     private String firstName;
     private String lastName;
     private String id; // Has to be a string in case of leading 0s.
-    
+
     /**
-     * Construct an empty student record.
+     * The student's first name, as processed by the OCR software.
      */
-    public Student() {}
-    
+    private final StochasticString stochasticFirst;
+
     /**
-     * Construct a student record given a first name, last name, and 
+     * The student's last name, as processed by the OCR software.
+     */
+    private final StochasticString stochasticLast;
+
+    BufferedImage firstNameImage;
+    BufferedImage lastNameImage;
+
+    /**
+     * Construct a student record given a first name, last name, and
      * identification number.
-     * 
+     *
      * @param firstName A string representing the first name of the student.
      * @param lastName A string representing the last name of the student.
      * @param id A string representing the identification number of the student.
      */
-    public Student(String firstName, String lastName, String id) 
+    public Student(String firstName, String lastName, String id,
+            BufferedImage firstNameImg, BufferedImage lastNameImg)
     {
         // SET the firstName field to firstName
         // SET the lastName field to lastName
         // SET the id field to id
-        
+
         this.firstName = firstName;
         this.lastName = lastName;
         this.id = id;
+        this.stochasticFirst = null;
+        this.stochasticLast = null;
+        this.firstNameImage = firstNameImg;
+        this.lastNameImage = lastNameImg;
     }
-    
+
+    public Student(StochasticString first, StochasticString last,
+            BufferedImage firstNameImg, BufferedImage lastNameImg)
+    {
+        super();
+        this.stochasticFirst = first;
+        this.stochasticLast = last;
+        this.firstNameImage = firstNameImg;
+        this.lastNameImage = lastNameImg;
+    }
+
     /**
      * Return the first name in the student record.
-     * 
+     *
      * @return The first name in the student record.
      */
-    public String getFirstName() 
+    public String getFirstName()
     {
         // RETURN firstName  
-        
+
         return firstName;
     }
-    
+
     /**
      * Return the last name in the student record.
-     * 
+     *
      * @return The last name in the student record.
      */
-    public String getLastName() 
+    public String getLastName()
     {
         // RETURN lastName  
-        
+
         return lastName;
     }
-    
+
     /**
      * Return the identification number in the student record.
-     * 
+     *
      * @return The identification number in the student record.
      */
-    public String getId() {
+    public String getId()
+    {
         // RETURN id  
-        
+
         return id;
     }
-    
+
     /**
      * Set the first name in the student record.
-     * 
+     *
      * @param firstName The first name in the student record.
      */
-    public void setFirstName(String firstName) { 
+    public void setFirstName(String firstName)
+    {
         // SET the firstName field to firstName
-        
+
         this.firstName = firstName;
     }
-    
+
     /**
      * Set the last name in the student record.
-     * 
+     *
      * @param lastName The last name in the student record.
      */
-    public void setlastName(String lastName) { 
+    public void setlastName(String lastName)
+    {
         // SET the lastName field to lastName
-        
+
         this.lastName = lastName;
     }
-    
+
     /**
      * Set the identification number in the student record.
-     * 
+     *
      * @param id The identification number in the student record.
      */
-    public void setId(String id) {
+    public void setId(String id)
+    {
         // SET the lastName id to id
-        
+
         this.id = id;
+    }
+
+    public StochasticString getStochasticFirst()
+    {
+        return this.stochasticFirst;
+    }
+
+    public StochasticString getStochasticLast()
+    {
+        return this.stochasticLast;
     }
 }
