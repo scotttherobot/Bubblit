e0d1ab4ef3b1116cce2403e7d2784095bbbd25a6
VeryBueno <lcuellar@calpoly.edu>
2015-05-31 02:43:52 +0000

    InputFileList : checkstyle

--- ExamGrader/src/com/universalquantification/examgrader/models/InputFileList.java
+++ ExamGrader/src/com/universalquantification/examgrader/models/InputFileList.java
@@ -10,43 +10,50 @@ import java.util.List;
 
 /**
  * An InputFileList represents a list of InputFiles.
+ *
  * @author Jenny Wang
  * @version 2.0
  */
+public class InputFileList extends Observable
+{
 
-public class InputFileList extends Observable {
-    
     private List<InputFile> files;
-    
+
+    /**
+     * Construct a new InputFileList
+     */
     public InputFileList()
     {
-        files = new ArrayList<>();
+        files = new ArrayList<InputFile>();
     }
-    
+
     /**
      * Returns the paths of all the files in the list
+     *
      * @return paths of all the files in the list
      */
     public List<File> getFiles()
     {
         // RETURN files
-        List<File> fileNames = new ArrayList<>();
+        List<File> fileNames = new ArrayList<File>();
+        // go through every file and add it to the list
         for (InputFile file : files)
         {
             fileNames.add(file.getFileName());
         }
         return fileNames;
     }
-    
+
     /**
      * Returns all the input files in the list.
+     *
      * @return all the input files in the list
      */
     public List<InputFile> getInputFiles()
     {
         return files;
     }
-    
+
     /**
      * Clears the input file list
      */
@@ -59,9 +66,10 @@ public class InputFileList extends Observable {
         // CALL notifyObservers
         notifyObservers();
     }
-    
+
     /**
      * Returns the sum of all the pages in the InputFiles
+     *
      * @return total number of pages
      */
     public int getTotalPages()
@@ -72,15 +80,15 @@ public class InputFileList extends Observable {
         {
             total += file.getNumPages();
         }
-        
+
         return total;
     }
-    
+
     /**
      * Adds an input file to the list and notifies observers of changes.
+     *
      * @param file File path to add
      * @throws IOException if the file could not be read
-     * @throws FileFormatException if the file was of the wrong format
      */
     public void addInputFile(File file) throws IOException
     {
@@ -88,30 +96,30 @@ public class InputFileList extends Observable {
         InputFile inputFile = new InputFile(file);
         // Add files to inputFiles
         files.add(inputFile);
-        
+
         // CALL setChanged
         setChanged();
         // CALL notifyObservers
         notifyObservers();
     }
-    
+
     /**
-     * Deletes the input file at place ndx from the list
-     * of input files for processing.
+     * Deletes the input file at place ndx from the list of input files for
+     * processing.
+     *
      * @param ndx int index of the input file
-     * @throws IndexOutOfBounds if there is no file at ndx
      */
     public void deleteInputFile(int ndx)
     {
         // Delete ndx'th element in inputFiles
-        
+
         files.remove(ndx);
-        
+
         // CALL setChanged
         // CALL notifyObservers
         setChanged();
         notifyObservers();
-        
+
     }
-    
+
 }
