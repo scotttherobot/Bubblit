d88787d6fb27ffcc844eb8c6316fadeffc0dda00
VeryBueno <lcuellar@calpoly.edu>
2015-06-01 05:10:06 +0000

    AppViewExceptionHandler : checkstyle

--- ExamGrader/src/com/universalquantification/examgrader/ui/AppViewExceptionHandler.java
+++ ExamGrader/src/com/universalquantification/examgrader/ui/AppViewExceptionHandler.java
@@ -5,22 +5,34 @@
 package com.universalquantification.examgrader.ui;
 
 /**
- *
+ * Handles an exception from an {@link AppView}
  * @author jenwang
  */
-public class AppViewExceptionHandler implements Thread.UncaughtExceptionHandler{
+public class AppViewExceptionHandler implements Thread.UncaughtExceptionHandler
+{
 
     private AppView appView;
-    
-    public AppViewExceptionHandler(AppView appView) {
+
+    /**
+     * Create a new exception handler for the given {@link AppView}
+     * @param appView the AppView to handle exceptions for
+     */
+    public AppViewExceptionHandler(AppView appView)
+    {
         this.appView = appView;
     }
 
+    /**
+     * Notify the user that an unhandled exception was thrown
+     * @param thread the thread it occurred on
+     * @param thrwbl what was thrown
+     */
     @Override
-    public void uncaughtException(Thread thread, Throwable thrwbl) {
+    public void uncaughtException(Thread thread, Throwable thrwbl)
+    {
         thrwbl.printStackTrace();
         appView.showError("We received an unexpected error. Please restart and"
-                + "try again.");
+            + "try again.");
     }
-    
+
 }
