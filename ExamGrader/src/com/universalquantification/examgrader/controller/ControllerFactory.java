0efd2b8e59d36c59ca0a81ea122975314a78703f
VeryBueno <lcuellar@calpoly.edu>
2015-06-01 04:48:45 +0000

    ControllerFactory : checkstyle

--- ExamGrader/src/com/universalquantification/examgrader/controller/ControllerFactory.java
+++ ExamGrader/src/com/universalquantification/examgrader/controller/ControllerFactory.java
@@ -8,14 +8,22 @@ package com.universalquantification.examgrader.controller;
 import com.universalquantification.examgrader.ui.AppView;
 
 /**
+ * Create a new controller
  *
  * @author jenny
  */
-public class ControllerFactory {
-    
+public class ControllerFactory
+{
+
+    /**
+     * Create a new controller with a given {@link AppView}
+     *
+     * @param view the {@link AppView} to use
+     * @return the controller
+     */
     public Controller buildController(AppView view)
     {
         return new Controller(view);
     }
-    
+
 }
