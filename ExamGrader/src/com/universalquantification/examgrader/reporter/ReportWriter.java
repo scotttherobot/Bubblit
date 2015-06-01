ad55413140773288895bdfe63aefa36ec7cc590b
VeryBueno <lcuellar@calpoly.edu>
2015-06-01 04:42:08 +0000

    ReportWrite : checkstyle

--- ExamGrader/src/com/universalquantification/examgrader/reporter/ReportWriter.java
+++ ExamGrader/src/com/universalquantification/examgrader/reporter/ReportWriter.java
@@ -5,41 +5,39 @@ import com.universalquantification.examgrader.models.Exam;
 import com.universalquantification.examgrader.models.GradedExamCollection;
 import com.universalquantification.examgrader.utils.PreferencesManager;
 import java.io.File;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.nio.file.Paths;
 import java.util.Map;
 
 /**
  * a ReportWriter is a dispatcher for writing reports for a map of
  * GradedExamCollections.
- * 
+ *
  * @author Scott Vanderlind
  * @date 2015-05-10
  * @version 2.0
  */
 public class ReportWriter
 {
-    
-    private static final String RESOURCE_PATH = "resources";
-    
+
+    private static final String kResourcePath = "resources";
+
     /**
-     * Instantiate a writer to a particular file path.
-     * If outputDirectoryOverride is not set, the default output path will
-     * be used.
+     * Instantiate a writer to a particular file path. If
+     * outputDirectoryOverride is not set, the default output path will be used.
      */
     public ReportWriter()
     {
     }
-    
+
     private File getOutputDirectory(File file)
     {
         File parent;
-        
+
         Object outputDirectory = PreferencesManager.getInstance().get(
             PreferencesManager.OVERRIDE_DIR);
+        // make sure that the output directory exists
         if (outputDirectory == null)
         {
             parent = file.getParentFile();
@@ -50,63 +48,71 @@ public class ReportWriter
         }
         return parent;
     }
-    
+
     /**
-     * Writes the output reports given a map of GradedExamCollections.
-     * For each GradedExamCollection, an AggregateReport is created and written,
-     * and for each Exam in that GradedExamCollection an ExamReport is
-     * created and written.
+     * Writes the output reports given a map of GradedExamCollections. For each
+     * GradedExamCollection, an AggregateReport is created and written, and for
+     * each Exam in that GradedExamCollection an ExamReport is created and
+     * written.
+     *
      * @param collection - graded exams
-     * @throws IOException 
+     * @throws IOException
      */
-    public void writeReports(Map<File, GradedExamCollection> collection) throws IOException
+    public void writeReports(Map<File, GradedExamCollection> collection) throws
+        IOException
     {
         // FOR EACH file, examCollection in the collection map
         for (Map.Entry<File, GradedExamCollection> entry : collection.entrySet())
         {
-            File f = entry.getKey();
+            File file = entry.getKey();
             // CALL getOutputDirectory AS f
-            File outputDirectory = getOutputDirectory(f);
+            File outputDirectory = getOutputDirectory(file);
             GradedExamCollection gradedExams = entry.getValue();
-        
+
             // INIT a new FileWriter with the destination path
             // Create the output directory for the HTML reports
-            String nameWithoutExtension = Files.getNameWithoutExtension(f.getName());
-            FileWriter outfile = new FileWriter(new File(outputDirectory, nameWithoutExtension + "_aggregate.csv"));
+            String nameWithoutExtension = 
+                Files.getNameWithoutExtension(file.getName());
+            FileWriter outfile = new FileWriter(new File(outputDirectory,
+                nameWithoutExtension + "_aggregate.csv"));
             // INIT a new FileReader with the template path
-   
-            InputStreamReader aggregateTemplate = new InputStreamReader(this.getClass().getResourceAsStream(
-                RESOURCE_PATH + "/aggregate_report.csv"));
+
+            InputStreamReader aggregateTemplate = 
+                new InputStreamReader(this.getClass().getResourceAsStream(
+                    kResourcePath + "/aggregate_report.csv"));
             //FileReader aggregateTemplate = new FileReader(ior);
             // INIT a new AggregateReport with file + "_aggregate", examCollection
-            AggregateReport ar = new AggregateReport(gradedExams, outfile, aggregateTemplate);
+            AggregateReport ar = new AggregateReport(gradedExams, outfile,
+                aggregateTemplate);
             // CALL AggregateReport.writeReport
             ar.writeReport();
-            
-            File outfolder = new File(outputDirectory, nameWithoutExtension + "_www");
+
+            File outfolder = new File(outputDirectory, nameWithoutExtension
+                + "_www");
             outfolder.mkdir();
-            
+
             // FOR EACH exam in the examCollection
             for (Exam graded : gradedExams.getGradedExams())
             {
                 // INIT a new File with the destination path
                 File location = new File(outfolder,
-                        graded.getStudentRecord().getId() + ".html");
+                    graded.getStudentRecord().getId() + ".html");
                 // INIT a new FileWriter with the destination path
                 FileWriter examReportOut = 
-                 new FileWriter(location.getAbsolutePath());
+                    new FileWriter(location.getAbsolutePath());
                 // INIT a new FileReader with the template path
-                //FileReader examReportTemplate = new FileReader("src/com/universalquantification/examgrader/reporter/resources/exam_report.html");
-                InputStreamReader examReportTemplate = new InputStreamReader(this.getClass().getResourceAsStream(
-                RESOURCE_PATH + "/exam_report.html"));
+                InputStreamReader examReportTemplate = new InputStreamReader(
+                    this.getClass().getResourceAsStream(
+                        kResourcePath + "/exam_report.html"));
                 // INIT a new ExamReport with exam, file + "_" + exam.student.id
-                ExamReport eReport = new ExamReport(graded, examReportOut, examReportTemplate);
+                ExamReport eReport = new ExamReport(graded, examReportOut,
+                    examReportTemplate);
                 // CALL ExamReport.writeReport
                 eReport.writeReport();
-            // END FOR
+                // END FOR
             }
-        // END FOR
+            // END FOR
         }
     }
-    
+
 }
