51fbf33e5df8297ae905a57b8a90dc4e3846064c
VeryBueno <lcuellar@calpoly.edu>
2015-06-01 04:33:30 +0000

    ExamReport : stylecheck

--- ExamGrader/src/com/universalquantification/examgrader/reporter/ExamReport.java
+++ ExamGrader/src/com/universalquantification/examgrader/reporter/ExamReport.java
@@ -10,12 +10,7 @@ import com.github.mustachejava.Mustache;
 import com.github.mustachejava.MustacheFactory;
 import com.universalquantification.examgrader.models.Exam;
 import com.universalquantification.examgrader.utils.PreferencesManager;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.io.Reader;
 import java.io.StringWriter;
 import java.io.Writer;
@@ -24,9 +19,8 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 /**
- * an ExamReport is a per-student report of exam results.
- * Takes an instance of an Exam object and writes an HTML
- * document showing the results.
+ * an ExamReport is a per-student report of exam results. Takes an instance of
+ * an Exam object and writes an HTML document showing the results.
  *
  * @author Scott Vanderlind
  * @date 2015-05-10
@@ -38,27 +32,28 @@ public class ExamReport
      * The exam for which a report will be written.
      */
     private Exam exam;
-    
+
     /**
      * The destination file the report will be written to.
      */
     private Writer outfile;
-    
+
     /**
      * The variables that will be put into the template.
      */
     private HashMap<String, Object> scope;
-    
+
     /**
      * The template reader.
      */
     private Reader template;
 
     /**
-     * Instantiates a new ExamReport given a graded exam and a file
-     * to write to
+     * Instantiates a new ExamReport given a graded exam and a file to write to
+     *
      * @param exam - the graded exam
      * @param writeFile - the output file
+     * @param template the template to use
      */
     public ExamReport(Exam exam, Writer writeFile, Reader template)
     {
@@ -71,16 +66,19 @@ public class ExamReport
         // CALL scope.put with "exam" and this.exam
         this.scope = new HashMap<String, Object>();
         this.scope.put("exam", this.exam);
-        
+
         // We also need to inject the display preferences into the context.
-        boolean showCorrect = (boolean)PreferencesManager.getInstance().get("show-correct-answers");
+        boolean showCorrect = (boolean) PreferencesManager.getInstance().get(
+            "show-correct-answers");
         this.scope.put("showCorrect", showCorrect);
-        boolean showInorrect = (boolean)PreferencesManager.getInstance().get("show-incorrect-answers");
+        boolean showInorrect = (boolean) PreferencesManager.getInstance().get(
+            "show-incorrect-answers");
         this.scope.put("showIncorrect", showInorrect);
-        boolean showFullImage = (boolean)PreferencesManager.getInstance().get("show-full-image");
+        boolean showFullImage = (boolean) PreferencesManager.getInstance().get(
+            "show-full-image");
         this.scope.put("showFullImage", showFullImage);
     }
-    
+
     /**
      * Writes the HTML report to file
      */
@@ -100,29 +98,30 @@ public class ExamReport
         catch (IOException ex)
         {
             Logger.getLogger(ExamReport.class.getName()).log(Level.SEVERE, null,
-                    ex);
+                ex);
         }
     }
-    
+
     /**
      * Renders the HTML and returns it as a string.
+     *
      * @return the rendered html
      */
     public String renderHTML()
     {
         // INIT a new StringWriter
         StringWriter html = new StringWriter();
-       
+
         // INIT a new MustacheFactory mf
         MustacheFactory mf = new DefaultMustacheFactory();
         // SET generator to mf.compile with examReportTemplateName
-        Mustache m = mf.compile(this.template, "Exam Report");
+        Mustache mustache = mf.compile(this.template, "Exam Report");
         // CALL generator.execute with outfile and scope
-        m.execute(html, this.scope);
+        mustache.execute(html, this.scope);
         // CALL writer.flush
         html.flush();
-        
+
         return html.toString();
     }
-    
+
 }
