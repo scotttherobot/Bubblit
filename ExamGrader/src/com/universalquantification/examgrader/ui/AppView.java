224b988dc80d7e4d611c8fcc77c23da23717f511
VeryBueno <lcuellar@calpoly.edu>
2015-06-01 04:58:07 +0000

    AppView : checkstyle

--- ExamGrader/src/com/universalquantification/examgrader/ui/AppView.java
+++ ExamGrader/src/com/universalquantification/examgrader/ui/AppView.java
@@ -9,23 +9,27 @@ import java.util.Observer;
 
 /**
  * An interface representing a view for the grader application.
- * 
+ *
  * @author Jenny Wang
  * @version 2.0
  */
-public interface AppView extends Observer {
-    
+public interface AppView extends Observer
+{
+
     /**
      * Shows an error appropriate to the type of the view.
+     *
      * @param error error message to show.
      */
     public void showError(String error);
-    
+
     /**
      * Verify that the exams are labeled with the correct matches.
+     *
      * @param results results to check
      * @param roster roster to use
      */
-    public void checkRoster(Map<File, GradedExamCollection> results, List<RosterEntry> roster);
-    
+    public void checkRoster(Map<File, GradedExamCollection> results,
+        List<RosterEntry> roster);
+
 }
