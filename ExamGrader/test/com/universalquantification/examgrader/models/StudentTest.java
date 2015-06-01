e3ae5b52b5701d2b73ded8a1679f6ee92d8312e8
Jenny Wang <ywang227@gmail.com>
2015-06-01 07:35:13 +0000

    StudentTest: add extra tests to StudentTest

--- ExamGrader/test/com/universalquantification/examgrader/models/StudentTest.java
+++ ExamGrader/test/com/universalquantification/examgrader/models/StudentTest.java
@@ -17,18 +17,22 @@ public class StudentTest extends TestCase {
         Student s = createStudent("Al", "B. Baque");
     
         assertTrue(s.getFirstName().equals("Al"));
+        s.setFirstName("Yule");
+        assertEquals(s.getFirstName(), "Yule");
     }
     
     public void testGetLastName() {
         Student s = createStudent("Wyso", "Searius");
     
         assertTrue(s.getLastName().equals("Searius"));
+        s.setlastName("Sirius");
+        assertEquals(s.getLastName(), "Sirius");
     }
     
     public void testGetId() {
         Student s = createStudent("Mipe", "Reshus");
     
-        assertTrue(s.getId().equals("id Reshus"));
+        assertTrue(s.getId().equals("id Reshus"));;
     }
     
     public void testSetId() {
@@ -37,5 +41,17 @@ public class StudentTest extends TestCase {
         s.setId("Axe");
         
         assertTrue(s.getId().equals("Axe"));
+    }
+    
+    public void testSetConfidence() {
+        Student s = createStudent("asdf", "asdf");
+        s.setConfidence(1.99);
+        assertEquals(s.getConfidence(), 1.99, 0.01);
+    }
+    
+    public void testImages() {
+        Student s = createStudent("asdf", "asdf");
+        assertNotNull(s.getFirstNameImage());
+        assertNotNull(s.getLastNameImage());
     }
 }
